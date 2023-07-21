75aa2a3a9952b55fda889549f8eed8c89e11bb00
dbquiroga <day@xcapit.com>
2023-07-20 21:57:17 -0300

    test with POM and fixture

--- cypress/e2e/C001-comboBox.cy.js
+++ cypress/e2e/C001-comboBox.cy.js
@@ -1,22 +1,31 @@
+import personalData from "../fixtures/C001-personalDataComboBox.json";
+import { comboboxPage } from "../support/pages/C001-comboBox.page";
+
 describe("Llenar formulario ComboBox", () => {
-  before(() => {
-    // runs once before all tests
-    cy.visit("/combobox-dependiente/");
-    cy.get("#menu-item-178").next().trigger("mouseover");
-    cy.get(".et-show-dropdown").should("be.visible", { timeout: 7000 });
-    cy.wait(3000);
-  });
+  // Ir a la web https://testingqarvn.com.es
+  // Ingresar al menú “Prácticas QA”
+  // Seleccionar “Combobox dependiente”
+  // Completar todos los campos con datos válidos
+  //    - email con formato @...com
+  //    - nombre y apellido “reales”
+  // Hacer click en el botón “submit”
   beforeEach(() => {
     // runs before every it() test block
-  });
-  afterEach(() => {
-    // runs after every it() test block
-  });
-  after(() => {
-    // runs once after all tests
+    cy.visit("/combobox-dependiente/");
   });
   // -- Start: Cypress Tests --
   it("llenar el formulario exitosamente", () => {
-    // Write your test case one here
+    cy.get(comboboxPage.inputName).type(personalData.name);
+    cy.get(comboboxPage.inputLastName).type(personalData.lastName);
+    cy.get(comboboxPage.inputEmail).type(personalData.email);
+    cy.get(comboboxPage.inputPhone).type(personalData.phone);
+    cy.get(comboboxPage.direction).type(personalData.direccion);
+    cy.get(comboboxPage.inputPHP).click();
+    cy.get(comboboxPage.submitBtn).click();
+
+    cy.get(comboboxPage.alertSucess)
+      .should("be.visible", { timeout: 7000 })
+      .invoke("text")
+      .should("contain", "Gracias por tu encuesta.");
   });
 });
---
+++ cypress/support/pages/C001-comboBox.page.js
@@ -0,0 +1,16 @@
+class Combobox {
+  constructor() {
+    this.inputName = "#wsf-1-field-54";
+    this.inputLastName = "#wsf-1-field-55";
+    this.inputEmail = "#wsf-1-field-56";
+    this.inputPhone = "#wsf-1-field-57";
+    this.direction = "#wsf-1-field-58";
+    this.inputPHP = "#wsf-1-label-59-row-1";
+    this.inputPython = "#wsf-1-label-60-row-2";
+    this.submitBtn = "#wsf-1-field-62";
+
+    this.alertSucess = ".wsf-alert-success";
+  }
+}
+
+export const comboboxPage = new Combobox();
